3744be2a1636e247a2e345e7c48dc89fe69ca48f
Fabian <fabian.91m@gmail.com>
2025-03-24 08:28:50 +0100

    Added initial components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatIconButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatIconButton, MatIcon, FlexLayoutModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
