885b6bac3db395f71b0af91df5589f93746668bf
Fabian Meier <fabian.91m@gmail.com>
2025-07-21 11:43:20 +0200

    Add install button, remove sidenav focus.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Component, ElementRef, Inject, OnInit, PLATFORM_ID, ViewChild} from '@angular/core';
 import {Router, RouterModule} from '@angular/router';
-import {AsyncPipe} from '@angular/common';
+import {isPlatformBrowser} from '@angular/common';
 import {MatToolbar} from "@angular/material/toolbar";
-import {MatIconButton} from "@angular/material/button";
+import {MatButtonModule, MatIconButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
 import {FlexLayoutModule} from "@angular/flex-layout";
 import {MatSidenavModule} from "@angular/material/sidenav";
@@ -16,18 +16,34 @@ import {MatBadge} from "@angular/material/badge";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, MatToolbar, MatIconButton, MatIcon, FlexLayoutModule,
-    FlexLayoutServerModule, MatSidenavModule, MatListModule, AsyncPipe, MatBadge],
+  imports: [RouterModule, MatToolbar, MatIconButton, MatIcon, FlexLayoutModule, MatButtonModule,
+    FlexLayoutServerModule, MatSidenavModule, MatListModule, MatBadge],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  installPrompt?: any;
   @ViewChild('adminInput') readonly adminInput?: ElementRef;
   adminCount = 0;
 
   constructor(public readonly router: Router, public readonly $broadcast: BroadcastService,
               private readonly cookieService: CookieService,
-              public readonly properties: PropertiesService) {
+              public readonly properties: PropertiesService,
+              @Inject(PLATFORM_ID) private readonly platformId: Object) {
+  }
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      window.addEventListener('beforeinstallprompt', e =>
+        (this.installPrompt = e) && e.preventDefault());
+      window.addEventListener('appinstalled', () => this.installPrompt = undefined);
+    }
+  }
+
+  install() {
+    this.installPrompt!.prompt();
+    this.installPrompt!.userChoice.then((result: string) =>
+      result == "accepted" && (this.installPrompt = undefined));
   }
 
   admin() {
